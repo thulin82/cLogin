4b08eb204f12588a09bfe4abde83e8cf5a9f30b4
Markus Thulin <macky_b@hotmail.com>
2019-05-24 15:41:04 +0200

    Adding tests to ConnectTest.php (blindly...)

--- test/ConnectTest.php
+++ test/ConnectTest.php
@@ -23,4 +23,21 @@ class ConnectTest extends \PHPUnit\Framework\TestCase
         $var = $this->_test->exists("person");
         $this->assertEquals($var, false, "Negative AddUser failed");
     }
+
+    public function testDeleteUser()
+    {
+        $this->_test->addUser("user", "name", "pass");
+        $var = $this->_test->exists("user");
+        $this->assertEquals($var, true, "AddUser failed");
+        $this->_test->deleteUser("user");
+        $var = $this->_test->exists("user");
+        $this->assertEquals($var, false, "DeleteUser failed");
+    }
+
+    public function testGetHash()
+    {
+        $this->_test->addUser("user", "name", "pass");
+        $var = $this->_test->getHash("user");
+        $this->assertNotEquals($var, "pass", "Hash and password equal");
+    }
 }
